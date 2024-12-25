a1ceb1ad84d34d5078ecc5d9d59373f45c0ff71c
cgebhart-lbl <cgebhart@lbl.gov>
2024-12-25 05:37:17 -0800

    Fixed a compilation error in CardZone

--- Assets/Scripts/Engine/CardZone.cs
+++ Assets/Scripts/Engine/CardZone.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class CardZone
 {
     public CardZoneName name {get; private set;}
+    public int player {get; private set;}
     public List<Card> cards { get; private set;}
-    public CardZone(CardZoneName name_, Player.ID )
+    public CardZone(CardZoneName name_, int playerID_)
     {
         name = name_;
+        player = playerID_;
     }
 }
